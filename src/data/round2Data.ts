3707e7f0109087a1739b8ee976b6bf651187fa97
dientruonggg <dientruong2104@gmail.com>
2026-08-05 01:26:39 +0700

    Fix image paths for Best Member and Rookie to match uploaded assets

--- src/data/round2Data.ts
+++ src/data/round2Data.ts
@@ -11,29 +11,29 @@ export interface Top5Candidate {
 
 export const TOP_5_BEST_MEMBERS: Top5Candidate[] = [
   // BANANA
-  { id: 'top5_member_1', name: 'Nguyễn Trần Uyên Trang', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/ntut.png' },
-  { id: 'top5_member_2', name: 'Bạch Nhật Minh', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/bnm.png' },
-  { id: 'top5_member_3', name: 'Mai Vũ Phúc', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/mvp.png' },
-  { id: 'top5_member_4', name: 'Hồ Thị Mỹ Duyên', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/htmd.png' },
-  { id: 'top5_member_5', name: 'Phạm Phan Bảo Trúc', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/ppbt.png' },
+  { id: 'top5_member_1', name: 'Nguyễn Trần Uyên Trang', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/Banana/ntutrang.JPG' },
+  { id: 'top5_member_2', name: 'Bạch Nhật Minh', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/Banana/bnminh.JPG' },
+  { id: 'top5_member_3', name: 'Mai Vũ Phúc', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/Banana/mvphuc.jpg' },
+  { id: 'top5_member_4', name: 'Hồ Thị Mỹ Duyên', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/Banana/htmduyen.jpg' },
+  { id: 'top5_member_5', name: 'Phạm Phan Bảo Trúc', teamId: 'bnn', image: '/assets/profile_image/TheBestMember/Banana/ppbtruc.jpg' },
   // NIFFLER
-  { id: 'top5_member_6', name: 'Lê Long Vũ', teamId: 'niff', image: '/assets/profile_image/TheBestMember/llv.png' },
-  { id: 'top5_member_7', name: 'Nguyễn Thị Thu Hiền', teamId: 'niff', image: '/assets/profile_image/TheBestMember/ntth.png' },
-  { id: 'top5_member_8', name: 'Trần Trương Đức Lộc', teamId: 'niff', image: '/assets/profile_image/TheBestMember/ttdl.png' },
-  { id: 'top5_member_9', name: 'Nguyễn Hữu Hòa Bình', teamId: 'niff', image: '/assets/profile_image/TheBestMember/nhhb.png' },
-  { id: 'top5_member_10', name: 'Trương Thị Trà My', teamId: 'niff', image: '/assets/profile_image/TheBestMember/tttm.png' },
+  { id: 'top5_member_6', name: 'Lê Long Vũ', teamId: 'niff', image: '/assets/profile_image/TheBestMember/Niffler/llvu.jpg' },
+  { id: 'top5_member_7', name: 'Nguyễn Thị Thu Hiền', teamId: 'niff', image: '/assets/profile_image/TheBestMember/Niffler/ntthien.jpg' },
+  { id: 'top5_member_8', name: 'Trần Trương Đức Lộc', teamId: 'niff', image: '/assets/profile_image/TheBestMember/Niffler/ttdloc.jpg' },
+  { id: 'top5_member_9', name: 'Nguyễn Hữu Hòa Bình', teamId: 'niff', image: '/assets/profile_image/TheBestMember/Niffler/nhhbinh.JPG' },
+  { id: 'top5_member_10', name: 'Trương Thị Trà My', teamId: 'niff', image: '/assets/profile_image/TheBestMember/Niffler/tttmy.JPG' },
   // POWER RANGERS
-  { id: 'top5_member_11', name: 'Phạm Thanh Pha', teamId: 'prs', image: '/assets/profile_image/TheBestMember/ptp.png' },
-  { id: 'top5_member_12', name: 'Nguyễn Hữu Lập', teamId: 'prs', image: '/assets/profile_image/TheBestMember/nhl.png' },
-  { id: 'top5_member_13', name: 'Phạm Mạnh Dũng', teamId: 'prs', image: '/assets/profile_image/TheBestMember/pmd.png' },
-  { id: 'top5_member_14', name: 'Trần Dĩ Kha', teamId: 'prs', image: '/assets/profile_image/TheBestMember/tdk.png' },
-  { id: 'top5_member_15', name: 'Nguyễn Tấn Sinh Thời', teamId: 'prs', image: '/assets/profile_image/TheBestMember/ntst.png' },
+  { id: 'top5_member_11', name: 'Phạm Thanh Pha', teamId: 'prs', image: '/assets/profile_image/TheBestMember/Power_Rangers/ptpha.jpg' },
+  { id: 'top5_member_12', name: 'Nguyễn Hữu Lập', teamId: 'prs', image: '/assets/profile_image/TheBestMember/Power_Rangers/nhlap.PNG' },
+  { id: 'top5_member_13', name: 'Phạm Mạnh Dũng', teamId: 'prs', image: '/assets/profile_image/TheBestMember/Power_Rangers/pmdung.JPG' },
+  { id: 'top5_member_14', name: 'Trần Dĩ Kha', teamId: 'prs', image: '/assets/profile_image/TheBestMember/Power_Rangers/tdkha.jpg' },
+  { id: 'top5_member_15', name: 'Nguyễn Tấn Sinh Thời', teamId: 'prs', image: '/assets/profile_image/TheBestMember/Power_Rangers/ntsthoi.JPG' },
   // HEROES COMPANY
-  { id: 'top5_member_16', name: 'Hoàng Bảo Ngọc', teamId: 'hc', image: '/assets/profile_image/TheBestMember/hbn.png' },
-  { id: 'top5_member_17', name: 'Vũ Thanh Trà', teamId: 'hc', image: '/assets/profile_image/TheBestMember/vtt.png' },
-  { id: 'top5_member_18', name: 'Nguyễn Đại Phú', teamId: 'hc', image: '/assets/profile_image/TheBestMember/ndp.png' },
-  { id: 'top5_member_19', name: 'Trương Thị Ngọc Huyền', teamId: 'hc', image: '/assets/profile_image/TheBestMember/ttnh.png' },
-  { id: 'top5_member_20', name: 'Thái Thành Tài', teamId: 'hc', image: '/assets/profile_image/TheBestMember/ttt.png' }
+  { id: 'top5_member_16', name: 'Hoàng Bảo Ngọc', teamId: 'hc', image: '/assets/profile_image/TheBestMember/Heroes_Company/hbngoc.jpg' },
+  { id: 'top5_member_17', name: 'Vũ Thanh Trà', teamId: 'hc', image: '/assets/profile_image/TheBestMember/Heroes_Company/vttra.jpg' },
+  { id: 'top5_member_18', name: 'Nguyễn Đại Phú', teamId: 'hc', image: '/assets/profile_image/TheBestMember/Heroes_Company/ndphu.jpg' },
+  { id: 'top5_member_19', name: 'Trương Thị Ngọc Huyền', teamId: 'hc', image: '/assets/profile_image/TheBestMember/Heroes_Company/ttnhuyen.jpg' },
+  { id: 'top5_member_20', name: 'Thái Thành Tài', teamId: 'hc', image: '/assets/profile_image/TheBestMember/Heroes_Company/tttai.jpg' }
 ];
 
 export const TOP_5_BEST_EVENTS: Top5Candidate[] = [
@@ -45,11 +45,11 @@ export const TOP_5_BEST_EVENTS: Top5Candidate[] = [
 ];
 
 export const TOP_5_ROOKIES: Top5Candidate[] = [
-  { id: 'top5_rookie_1', name: 'Lê Long Vũ', teamId: 'niff', image: '/assets/profile_image/TheRookie/llv.png' },
-  { id: 'top5_rookie_2', name: 'Nguyễn Đại Phú', teamId: 'hc', image: '/assets/profile_image/TheRookie/ndp.png' },
-  { id: 'top5_rookie_3', name: 'Thái Thành Tài', teamId: 'hc', image: '/assets/profile_image/TheRookie/ttt.png' },
-  { id: 'top5_rookie_4', name: 'Huỳnh Thị Thanh Lịch', teamId: 'prs', image: '/assets/profile_image/TheRookie/httl.png' },
-  { id: 'top5_rookie_5', name: 'Nguyễn Tấn Sinh Thời', teamId: 'prs', image: '/assets/profile_image/TheRookie/ntst.png' }
+  { id: 'top5_rookie_1', name: 'Lê Long Vũ', teamId: 'niff', image: '/assets/profile_image/TheRookie/llvu.JPG' },
+  { id: 'top5_rookie_2', name: 'Nguyễn Đại Phú', teamId: 'hc', image: '/assets/profile_image/TheRookie/ndphu.jpg' },
+  { id: 'top5_rookie_3', name: 'Thái Thành Tài', teamId: 'hc', image: '/assets/profile_image/TheRookie/tttai.jpg' },
+  { id: 'top5_rookie_4', name: 'Huỳnh Thị Thanh Lịch', teamId: 'prs', image: '/assets/profile_image/TheRookie/httlich.PNG' },
+  { id: 'top5_rookie_5', name: 'Nguyễn Tấn Sinh Thời', teamId: 'prs', image: '/assets/profile_image/TheRookie/ntsthoi.JPG' }
 ];
 
 export const TOP_5_PERFECT_DUOS: Top5Candidate[] = [
